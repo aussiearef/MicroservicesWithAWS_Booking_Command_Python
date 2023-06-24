1c85414978be276df1bd5333e28830ec8fd2075a
aussiearef <arefkr@gmail.com>
2023-06-24 16:47:06 +1000

    Set the port to 80

--- Booking-Command.py
+++ Booking-Command.py
@@ -63,4 +63,4 @@ def health_check():
     return JSONResponse(status_code= 200, content="OK")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8004)
+    uvicorn.run(app, host="0.0.0.0", port=80)
